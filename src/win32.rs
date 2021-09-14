c2620e9b7ebb77fe01a51b52950197424ab99348
Horia Mut <horiamut@msn.com>
2021-09-14 20:50:57 +0500

    Separate win32 code from main
    Summary
    ------------
    Put all win32 related types, constants and external functions in it's
    own source file. Add lib.rs to use the win32 library only when on
    Windows.
    
    Refactoring
    ------------
    - Separate platform code from non platform code as much as possible.
    
    Known Issues
    ------------
    - `window_procedure` extern is still in `main.rs`.

---
+++ src/win32.rs
@@ -0,0 +1,335 @@
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+/// Following the tutorial :
+/// https://rust-tutorials.github.io/triangle-from-scratch/opening_a_window/win32.html
+use core::ffi::c_void;
+use std::os::raw::{c_int, c_uint};
+
+// See
+// - https://docs.microsoft.com/en-us/cpp/cpp/data-type-ranges?view=msvc-160
+// - https://docs.microsoft.com/en-us/windows/win32/winprog/windows-data-types
+// Headers potential location (depends on SDK installed):
+// C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um\WinUser.h
+
+// These are defined in std::os::raw.
+// type c_int = i32;
+// type c_uint = u32;
+
+pub type HANDLE = PVOID;
+pub type HINSTANCE = HANDLE;
+pub type HICON = HANDLE;
+pub type HCURSOR = HANDLE;
+pub type HBRUSH = HANDLE;
+pub type HWND = HANDLE;
+
+pub type LPCWSTR = *const WCHAR;
+pub type WCHAR = wchar_t;
+pub type wchar_t = u16; // Wide char, 2 bytes.
+
+pub type UINT = c_uint;
+pub type UINT_PTR = usize;
+pub type INT_PTR = isize;
+pub type LONG_PTR = UINT_PTR;
+
+pub type WPARAM = UINT_PTR;
+pub type LPARAM = LONG_PTR;
+pub type LRESULT = LONG_PTR;
+
+pub type PVOID = *mut c_void;
+
+
+pub type LONG = c_long;
+pub type c_long = i32;
+
+
+pub type HMODULE = HINSTANCE;
+pub type DWORD = c_ulong;
+pub type c_ulong = u32;
+
+pub type HDC = HANDLE;
+pub type BYTE = u8;
+
+pub type ATOM = WORD;
+pub type WORD = c_ushort;
+pub type c_ushort = u16;
+
+pub type HMENU = HANDLE;
+pub type LPVOID = *mut c_void;
+
+pub type BOOL = c_int;
+pub type LPMSG = *const MSG;
+
+pub type LPWSTR = *mut WCHAR;
+pub type ULONG_PTR = usize;
+
+pub type LPPAINTSTRUCT = *mut PAINTSTRUCT;
+
+
+///[`WNDPROC`](https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms633573(v=vs.85)) 
+/// This type defines a pointer to  the application-defined callback function `WindowProc` 
+/// that processes messages sent to a window.
+pub type WNDPROC = Option<
+    // WindowProcedure
+    unsafe extern "system" fn(hwnd: HWND, uMsg: UINT, wParam: WPARAM, lParam: LPARAM) -> LRESULT,
+>;
+
+/// Window Messages
+pub const WM_NULL: u32 = 0x0000;
+pub const WM_NCCREATE: u32 = 0x0081;
+pub const WM_CREATE: u32 = 0x0001;
+pub const WM_DESTROY: u32 = 0x0002;
+pub const WM_MOVE: u32 = 0x0003;
+pub const WM_SIZE: u32 = 0x0005;
+
+pub const WM_ACTIVATE: u32 = 0x0006;
+
+pub const WM_SETFOCUS: u32 = 0x0007;
+pub const WM_KILLFOCUS: u32 = 0x0008;
+pub const WM_ENABLE: u32 = 0x000A;
+pub const WM_SETREDRAW: u32 = 0x000B;
+pub const WM_SETTEXT: u32 = 0x000C;
+pub const WM_GETTEXT: u32 = 0x000D;
+pub const WM_GETTEXTLENGTH: u32 = 0x000E;
+pub const WM_PAINT: u32 = 0x000F;
+pub const WM_CLOSE: u32 = 0x0010;
+pub const WM_QUIT: u32 = 0x0012;
+pub const WM_ERASEBKGND: u32 = 0x0014;
+pub const WM_SYSCOLORCHANGE: u32 = 0x0015;
+pub const WM_SHOWWINDOW: u32 = 0x0018;
+pub const WM_WININICHANGE: u32 = 0x001A;
+pub const WM_DEVMODECHANGE: u32 = 0x001B;
+pub const WM_ACTIVATEAPP: u32 = 0x001C;
+pub const WM_FONTCHANGE: u32 = 0x001D;
+pub const WM_TIMECHANGE: u32 = 0x001E;
+pub const WM_CANCELMODE: u32 = 0x001F;
+pub const WM_SETCURSOR: u32 = 0x0020;
+pub const WM_MOUSEACTIVATE: u32 = 0x0021;
+pub const WM_CHILDACTIVATE: u32 = 0x0022;
+pub const WM_QUEUESYNC: u32 = 0x0023;
+pub const WM_GETMINMAXINFO: u32 = 0x0024;
+
+/// Window Styles
+pub const WS_OVERLAPPED: u32 = 0x00000000;
+pub const WS_POPUP: u32 = 0x80000000;
+pub const WS_CHILD: u32 = 0x40000000;
+pub const WS_MINIMIZE: u32 = 0x20000000;
+pub const WS_VISIBLE: u32 = 0x10000000;
+pub const WS_DISABLED: u32 = 0x08000000;
+pub const WS_CLIPSIBLINGS: u32 = 0x04000000;
+pub const WS_CLIPCHILDREN: u32 = 0x02000000;
+pub const WS_MAXIMIZE: u32 = 0x01000000;
+pub const WS_CAPTION: u32 = 0x00C00000; /* WS_BORDER | WS_DLGFRAME  */
+pub const WS_BORDER: u32 = 0x00800000;
+pub const WS_DLGFRAME: u32 = 0x00400000;
+pub const WS_VSCROLL: u32 = 0x00200000;
+pub const WS_HSCROLL: u32 = 0x00100000;
+pub const WS_SYSMENU: u32 = 0x00080000;
+pub const WS_THICKFRAME: u32 = 0x00040000;
+pub const WS_GROUP: u32 = 0x00020000;
+pub const WS_TABSTOP: u32 = 0x00010000;
+
+pub const WS_MINIMIZEBOX: u32 = 0x00020000;
+pub const WS_MAXIMIZEBOX: u32 = 0x00010000;
+
+pub const WS_TILED: u32 = WS_OVERLAPPED;
+pub const WS_ICONIC: u32 = WS_MINIMIZE;
+pub const WS_SIZEBOX: u32 = WS_THICKFRAME;
+pub const WS_TILEDWINDOW: u32 = WS_OVERLAPPEDWINDOW;
+
+/// Common Window Styles
+pub const WS_OVERLAPPEDWINDOW: u32 =
+    WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
+
+pub const WS_POPUPWINDOW: u32 = WS_POPUP | WS_BORDER | WS_SYSMENU;
+
+pub const WS_CHILDWINDOW: u32 = WS_CHILD;
+
+pub const CW_USEDEFAULT: c_int = 0x80000000_u32 as c_int;
+
+pub const SW_SHOW: c_int = 5;
+
+pub const IDC_ARROW: LPCWSTR = MAKEINTRESOURCE(32512);
+
+pub const COLOR_WINDOW: u32 = 5;
+
+pub const MB_OKCANCEL: u32 = 0x00000001;
+pub const IDOK: c_int = 1;
+
+pub const GWLP_USERDATA: c_int = -21;
+
+#[repr(C)] // Memory Layout : https://doc.rust-lang.org/reference/type-layout.html
+///[`WNDCLASSW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-wndclassw)
+pub struct WNDCLASSW {
+    pub style: UINT,
+    pub lpfnWndProc: WNDPROC,
+    pub cbClsExtra: c_int,
+    pub cbWndExtra: c_int,
+    pub hInstance: HINSTANCE,
+    pub hIcon: HICON,
+    pub hCursor: HCURSOR,
+    pub hbrBackground: HBRUSH,
+    pub lpszMenuName: LPCWSTR,
+    pub lpszClassName: LPCWSTR,
+}
+
+impl Default for WNDCLASSW {
+    #[must_use]
+    fn default() -> Self {
+        unsafe { core::mem::zeroed() }
+    }
+}
+
+macro_rules! unsafe_impl_default_zeroed {
+    ($t:ty) => {
+        impl Default for $t {
+            #[inline]
+            #[must_use]
+            fn default() -> Self {
+                unsafe { core::mem::zeroed() }
+            }
+        }
+    };
+}
+
+
+#[repr(C)]
+pub struct POINT {
+    pub x: LONG,
+    pub y: LONG,
+}
+unsafe_impl_default_zeroed!(POINT);
+
+#[repr(C)]
+pub struct MSG {
+    pub hwnd: HWND,
+    pub message: UINT,
+    pub wParam: WPARAM,
+    pub lParam: LPARAM,
+    pub time: DWORD,
+    pub pt: POINT,
+    pub lPrivate: DWORD,
+}
+unsafe_impl_default_zeroed!(MSG);
+
+
+#[repr(C)]
+pub struct RECT {
+    pub left: LONG,
+    pub top: LONG,
+    pub right: LONG,
+    pub bottom: LONG,
+}
+unsafe_impl_default_zeroed!(RECT);
+
+#[repr(C)]
+pub struct PAINTSTRUCT {
+    pub hdc: HDC,
+    pub fErase: BOOL,
+    pub rcPaint: RECT,
+    pub fRestore: BOOL,
+    pub fIncUpdate: BOOL,
+    pub rgbReserved: [BYTE; 32],
+}
+unsafe_impl_default_zeroed!(PAINTSTRUCT);
+
+#[repr(C)]
+pub struct CREATESTRUCTW {
+    pub lpCreateParams: LPVOID,
+    pub hInstance: HINSTANCE,
+    pub hMenu: HMENU,
+    pub hwndParent: HWND,
+    pub cy: c_int,
+    pub cx: c_int,
+    pub y: c_int,
+    pub x: c_int,
+    pub style: LONG,
+    pub lpszName: LPCWSTR,
+    pub lpszClass: LPCWSTR,
+    pub dwExStyle: DWORD,
+}
+unsafe_impl_default_zeroed!(CREATESTRUCTW);
+
+#[link(name = "Kernel32")]
+extern "system" {
+    /// [`GetModuleHandleW`](https://docs.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulehandlew)
+    pub fn GetModuleHandleW(lpModuleName: LPCWSTR) -> HMODULE;
+
+    /// [`GetLastError`](https://docs.microsoft.com/en-us/windows/win32/api/errhandlingapi/nf-errhandlingapi-getlasterror)
+    pub fn GetLastError() -> DWORD;
+}
+
+pub const fn MAKEINTRESOURCE(i: WORD) -> LPWSTR {
+    i as ULONG_PTR as LPWSTR
+}
+
+
+#[link(name = "User32")]
+extern "system" {
+    /// [`RegisterClassW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerclassw)
+    pub fn RegisterClassW(lpWndClass: *const WNDCLASSW) -> ATOM;
+
+    /// [`CreateWindowExW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexw#syntax)
+    pub fn CreateWindowExW(
+        dwExStyle: DWORD,
+        lpClassName: LPCWSTR,
+        lpWindowName: LPCWSTR,
+        dwStyle: DWORD,
+        X: c_int,
+        Y: c_int,
+        nWidth: c_int,
+        nHeight: c_int,
+        hWndParent: HWND,
+        hMenu: HMENU,
+        hInstance: HINSTANCE,
+        lpParam: LPVOID,
+    ) -> HWND;
+
+    /// [`ShowWindow`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow)
+    pub fn ShowWindow(hWnd: HWND, nCmdShow: c_int) -> BOOL;
+
+    /// [`DefWindowProcW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-defwindowprocw)
+    pub fn DefWindowProcW(hWnd: HWND, Msg: UINT, wParam: WPARAM, lParam: LPARAM) -> LRESULT;
+
+    ///[`GetMessageW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getmessagew)
+    pub fn GetMessageW(lpMsg: LPMSG, hWnd: HWND, wMsgFilterMin: UINT, wMsgFilterMax: UINT) -> BOOL;
+
+    /// [`TranslateMessage`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-translatemessage)
+    pub fn TranslateMessage(lpMsg: *const MSG) -> BOOL;
+
+    /// [`DispatchMessageW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-dispatchmessagew)
+    pub fn DispatchMessageW(lpMsg: *const MSG) -> LRESULT;
+
+    /// [`PostQuitMessage`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-postquitmessage)
+    pub fn PostQuitMessage(nExitCode: c_int);
+
+    /// [`DestroyWindow`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-destroywindow)
+    pub fn DestroyWindow(hWnd: HWND) -> BOOL;
+
+    /// [`LoadCursorW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-loadcursorw)
+    pub fn LoadCursorW(hInstance: HINSTANCE, lpCursorName: LPCWSTR) -> HCURSOR;
+
+    /// [`BeginPaint`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-beginpaint)
+    pub fn BeginPaint(hWnd: HWND, lpPaint: *mut PAINTSTRUCT) -> HDC;
+
+    /// [`FillRect`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-fillrect)
+    pub fn FillRect(hDC: HDC, lprc: *const RECT, hbr: HBRUSH) -> c_int;
+
+    /// [`EndPaint`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-endpaint)
+    pub fn EndPaint(hWnd: HWND, lpPaint: *const PAINTSTRUCT) -> BOOL;
+
+    /// [`MessageBoxW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messageboxw)
+    pub fn MessageBoxW(hWnd: HWND, lpText: LPCWSTR, lpCaption: LPCWSTR, uType: UINT) -> c_int;
+
+    /// [`SetWindowLongPtrW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowlongptrw)
+    pub fn SetWindowLongPtrW(hWnd: HWND, nIndex: c_int, dwNewLong: LONG_PTR) -> LONG_PTR;
+
+    /// [`GetWindowLongPtrW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowlongptrw)
+    pub fn GetWindowLongPtrW(hWnd: HWND, nIndex: c_int) -> LONG_PTR;
+
+}
+
+/// Turns a Rust string slice into a null-terminated utf-16 vector.
+pub fn wide_null(s: &str) -> Vec<u16> {
+    s.encode_utf16().chain(Some(0)).collect()
+}
